61f8a6e27914104d0044fceb357cead03ff33216
gomezzzer <gomezgeorge132@gmail.com>
2024-09-25 19:25:19 -0500

    imports

--- code/main.py
+++ code/main.py
@@ -13,14 +13,22 @@ surf = pygame.Surface((100, 200))
 surf.fill('orange') 
 x = 100
 
-# importing an image 
+# imports
 player_surf = pygame.image.load(join('../images/player.png')).convert_alpha() 
-star_surf = pygame.image.load(join('../images/star.png')).convert_alpha() 
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))  
+player_direction = -1
 
-# Generate random positions for 20 stars
+star_surf = pygame.image.load(join('../images/star.png')).convert_alpha() 
 star_positions = [(random.randint(0, WINDOW_WIDTH - star_surf.get_width()), 
                    random.randint(0, WINDOW_HEIGHT - star_surf.get_height())) 
                   for _ in range(20)]
+
+meteor_surf = pygame.image.load(join('../images/meteor.png')).convert_alpha() 
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)) 
+
+laser_surf = pygame.image.load(join('../images/laser.png')).convert_alpha() 
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))  
+
 
 while running:
     # event loop
@@ -31,12 +39,18 @@ while running:
     # Draw the game 
     display_surface.fill('darkgrey') 
     # Draw stars at their random positions
-    for pos in star_positions:
-        display_surface.blit(star_surf, pos)
-    x += 0.1
-    display_surface.blit(player_surf, (x, 150))  
+    for pos in star_positions: 
+        display_surface.blit(star_surf, pos) 
+            
+    display_surface.blit(meteor_surf, ( meteor_rect))   
+    display_surface.blit(laser_surf, ( laser_rect))   
     
-
+    #player movement 
+    player_rect.x += player_direction * 0.4
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:     
+       player_direction *= -1   
+    display_surface.blit(player_surf, ( player_rect)) 
+    
     
     pygame.display.update()      
 
