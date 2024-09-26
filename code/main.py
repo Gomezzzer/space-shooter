24a8f56a4cbe517ef3180acbc2e22fc902bd3139
gomezzzer <gomezgeorge132@gmail.com>
2024-09-26 11:50:33 -0500

    mouse motion

--- code/main.py
+++ code/main.py
@@ -18,7 +18,7 @@ x = 100
 # imports
 player_surf = pygame.image.load(join('../images/player.png')).convert_alpha() 
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))  
-player_direction = pygame.math.Vector2(1,0) 
+player_direction = pygame.math.Vector2(1,1) 
 player_speed = 300  
 
 star_surf = pygame.image.load(join('../images/star.png')).convert_alpha() 
@@ -39,19 +39,22 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+            print(1) 
+        if event.type == pygame.MOUSEMOTION:  
+            player_rect.center = event.pos   
+      
+               
     # Draw the game 
     display_surface.fill('darkgrey') 
     # Draw stars at their random positions
     for pos in star_positions: 
-        display_surface.blit(star_surf, pos) 
+      display_surface.blit(star_surf, pos) 
             
     display_surface.blit(meteor_surf, ( meteor_rect))   
     display_surface.blit(laser_surf, ( laser_rect))   
-    
-    #player movement 
-    player_rect.center += player_direction * player_speed * dt 
     display_surface.blit(player_surf, ( player_rect)) 
+    
     
     
     pygame.display.update()      
